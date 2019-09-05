1e45177693eb8aba2d3778d89dd3d5db69e1aca7
long charmroeun <longcharmroeun@gmail.com>
2019-09-05 14:38:36 +0700

    Add Method

--- Excel@PDF/ExcelToPDF.cs
+++ Excel@PDF/ExcelToPDF.cs
@@ -14,12 +14,28 @@ namespace Excel_PDF
     public class ExcelToPDF
     {
         private Workbook Workbook;
+        /// <summary>
+        /// Load Excel From Stream
+        /// </summary>
+        /// <param name="stream">Excel Data</param>
         public ExcelToPDF(Stream stream)
         {
             this.Workbook = new Workbook();
             this.Workbook.LoadFromStream(stream);
         }
-
+        /// <summary>
+        /// Load Excel From File
+        /// </summary>
+        /// <param name="file">Fileinfo</param>
+        public ExcelToPDF(FileInfo file)
+        {
+            this.Workbook = new Workbook();
+            this.Workbook.LoadFromFile(file.FullName);
+        }
+        /// <summary>
+        /// Save File Asynchronous
+        /// </summary>
+        /// <param name="filename">FileName</param>
         public void SaveFileAsync(string filename)
         {
             Task.Run(new Action(async () =>
@@ -35,7 +51,10 @@ namespace Excel_PDF
                 pdf.Save(filename);
             }));
         }
-
+        /// <summary>
+        /// Save File
+        /// </summary>
+        /// <param name="filename">FileName</param>
         public async void SaveFile(string filename)
         {
             List<Stream> streams = new List<Stream>();
@@ -47,6 +66,21 @@ namespace Excel_PDF
             }
             PdfDocument pdf = await this.CombinePDFAsyn(streams);
             pdf.Save(filename);
+        }
+        /// <summary>
+        /// Save PDF To Stream
+        /// </summary>
+        /// <returns></returns>
+        public async Task<PdfDocument> SaveToStreamAsync()
+        {
+            List<Stream> streams = new List<Stream>();
+            for (int i = 0; i < this.Workbook.Worksheets.Count; i++)
+            {
+                Stream stream = new MemoryStream();
+                this.Workbook.Worksheets[i].SaveToPdfStream(stream);
+                streams.Add(stream);
+            }
+            return await this.CombinePDFAsyn(streams);
         }
 
         private Task<PdfDocument> CombinePDFAsyn(List<Stream> streams)
---
+++ Test/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            var excel = new Spire.Xls.Workbook();
+            excel.LoadFromFile("../../test.xlsx");
+            Stream stream = new MemoryStream();
+            excel.SaveToStream(stream);
+            Excel_PDF.ExcelToPDF excelToPDF = new Excel_PDF.ExcelToPDF(stream);
+        }
+    }
+}
